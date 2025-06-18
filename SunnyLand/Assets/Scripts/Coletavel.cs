a5bb5694bee71be801ce1f82ad6a3a5c54f0fa61
FelZimmer <fozimermann@gmail.com>
2025-06-18 12:16:33 -0300

    stro18/06

--- SunnyLand/Assets/Scripts/Coletavel.cs
+++ SunnyLand/Assets/Scripts/Coletavel.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Coletavel : MonoBehaviour
+
+
 {
+    public TMP_Text texto_pontuacao;
+
     // Start is called before the first frame update
-    private int pontos = 5;
+    public ParticleSystem efeito;
+    private int pontos = 0;
     void Start()
     {
         Debug.Log("jogo começou, vc tem:\n" + pontos + "pontos!");
@@ -17,10 +22,24 @@ public class Coletavel : MonoBehaviour
     void Update()
     {
         //Debug.Log("jogo rodando");
+
     }
 
+    
 
 
+    private void OnTriggerEnter2D(Collider2D outro)
+    {
+        if (outro.CompareTag("Player"))
+        {
+            Debug.Log("Bateu");
+            outro.transform.localScale = new Vector3(1.5f,1.5f,1.5f);
+            Instantiate(efeito, transform.position, Quaternion.identity );
+            pontos++;
+            texto_pontuacao.text = "pontos:" + pontos.ToString();
+            Destroy(gameObject);
+        }
+    }
 
 
 }
